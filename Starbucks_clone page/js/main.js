35917cbc533c4beea8bdce286f79deb60225a0e5
Woo joung Yoon <34205465+woojoung1217@users.noreply.github.com>
2021-07-15 16:28:37 +0900

    update

--- Starbucks_clone page/js/main.js
+++ Starbucks_clone page/js/main.js
@@ -1,17 +1,41 @@
 const searchEl = document.querySelector('.search');
+//  .search 클래스를 찾아  => 변수안에 
 
 const searchInputEl = searchEl.querySelector('input');
+//찾은 변수안에서 input 클래스를 찾아 다른 변수 안에 넣는다.
 
 searchEl.addEventListener('click',function(){
   searchInputEl.focus();
-});
+}); // 이벤트 리스너 를 통해 클릭 될 경우 함수 실행시킨다.
 
 searchInputEl.addEventListener('focus',function(){
   searchEl.classList.add('focused');
   searchInputEl.setAttribute('placeholder', '통합검색');
-});
+}); // 요소가 focused 되면 placeholder 를 통해 검색 화면에 통합검색 을 표시한다.
 
 searchInputEl.addEventListener('blur',function(){
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
-});
+}); // 요소가 블러 처리 되면서 빈 화면이 나온다.
+
+const badgeEl = document.querySelector('header .badges');
+
+// throttle 로 함수 최적화 
+window.addEventListener('scroll',_.throttle(function(){
+  console.log(window.scrollY);
+  if (window.scrollY > 500){
+    //badge 숨기기 
+    //gsap.to(요소, 지속시간 , 옵션);
+    gsap.to(badgeEl, .6 , {
+      opacity: 0 ,
+      display:'none'
+    });
+  } else{
+    //badge 보이기
+    gsap.to(badgeEl, .6 ,{
+      opacity:1,
+      display:'block'
+    });
+  };
+}, 300));
+//_.throttle(함수, 시간) 300 = 0.3s
