7964cbdc442e8d880db992df549ac3d040ad2fb2
Woo joung Yoon <34205465+woojoung1217@users.noreply.github.com>
2021-07-20 16:53:05 +0900

    2021-07-21 Update

--- Starbucks_clone page/js/main.js
+++ Starbucks_clone page/js/main.js
@@ -57,3 +57,38 @@ new Swiper('.notice-line .swiper-container',{
   autoplay :true,
   loop:true
 });
+
+new Swiper('.promotion .swiper-container',{
+  slidesPerView :3,
+  spaceBetween:10,
+  centeredSlides:true,
+  loop:true,
+  // autoplay:{
+  //   delay: 3000,
+  // }
+  pagination: {
+    el: '.promotion .swiper-pagination', //페이지 번호요소 선택자 
+    clickable:true
+  },
+  navigation :{
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
+
+
+// 버튼 화면표시 영역 
+const promotionEl =document.querySelector('.promotion');
+const promotionToggleBtn =document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+
+promotionToggleBtn.addEventListener('click',function(){
+  isHidePromotion = !isHidePromotion
+  if(isHidePromotion){
+    //숨김처리 
+    promotionEl.classList.add('hide');
+  } else{
+    //화면 표시
+    promotionEl.classList.remove('hide');
+  }
+});
